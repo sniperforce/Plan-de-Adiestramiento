30f6b8685ea89a573d1dde3c79edf8b6f6238a81
sniperforce <diegozera9@gmail.com>
2025-10-01 13:00:11 -0400

    db changes

--- Semana 2,3 y 4/lib/config/db.js
+++ Semana 2,3 y 4/lib/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const ConnectDB = async () => {
   await mongoose.connect(
-    'mongodb+srv://diegozera9_db_user:gCImUBQthD3py0XL@cluster0.ylaqpdn.mongodb.net/ToDoApp'
+    'mongodb+srv://iuhubcz:afgazcbz@cluster0.ylaqpdn.mongodb.net/ToDoApp'
   );
   console.log('db connected');
 };
